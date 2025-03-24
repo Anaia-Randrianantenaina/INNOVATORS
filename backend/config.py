ce576a063a0cbb3bbb6e6d4198164973c11d163b
Judicael <judicaelsafidiniaina@gmail.com>
2025-03-24 15:04:26 +0300

    teste pour le git pull

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:246822@localhost:5432/hackaton'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:246822@localhost:5432/hackatons'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

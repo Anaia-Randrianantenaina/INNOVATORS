3a3d4d2992bf6176c5fb977826aaf07391827656
Andrianary <laurentantonio68@gmail.com>
2025-03-25 16:35:34 +0300

    documentation et mis à jour de la structure code Navbar

--- frontend/src/app/navigation/NavBar.jsx
+++ frontend/src/app/navigation/NavBar.jsx
@@ -7,8 +7,6 @@ import { RiChatFollowUpFill } from "react-icons/ri";
 import { MdOutlineDeliveryDining } from "react-icons/md"; 
 import { GrValidate } from "react-icons/gr"; 
 import { MdDashboard } from "react-icons/md"; 
-import { CiSquareQuestion } from "react-icons/ci"; 
-import { AiOutlineUser } from "react-icons/ai"; 
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
@@ -19,7 +17,7 @@ import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import { Avatar, Chip, ListItem, ListItemButton, ListItemIcon, ListItemText, responsiveFontSizes } from "@mui/material";
+import { Avatar, Badge, Chip, ListItem, ListItemButton, ListItemIcon, ListItemText, responsiveFontSizes } from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
 
 const drawerWidth = 240;
@@ -29,6 +27,7 @@ function NavBar(props) {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
 
+  // Donnée pour l'affichage dans le sidebar
   const navigation = [
     { path: "dashboard", name: "Dashboard", icon: <MdDashboard className="text-2xl" />  },
     { path: "demande", name: "Demande", icon: <BsFillChatSquareQuoteFill className="text-2xl" />  },
@@ -40,6 +39,7 @@ function NavBar(props) {
     { path: "user", name: "Utilisateur", icon:<FaUserAlt />  }
   ]
 
+  // Fonction pour fermer le sidebar quand la taille de l'écran est de version mobile
   const handleDrawerClose = () => {
     setIsClosing(true);
     setMobileOpen(false);
@@ -49,12 +49,14 @@ function NavBar(props) {
     setIsClosing(false);
   };
 
+  // Fonction pour ouvrir le sidebar quand la taille de l'écran est de version mobile
   const handleDrawerToggle = () => {
     if (!isClosing) {
       setMobileOpen(!mobileOpen);
     }
   };
 
+  // Fonction pour recupérer les données affiché dans le sidebar
   const drawer = (
     <div>
       <List>
@@ -76,34 +78,43 @@ function NavBar(props) {
     </div>
   );
 
-  // Remove this const when copying and pasting into your project.
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
+      {/* ---------------------------------Appbar qui contient le logo/notification-------------------------------- */}
       <AppBar position="fixed"
         sx={{ borderBottom: "1px solid #e0e0d1" }}
       >
         <Toolbar sx={{ background: "white", color: "black" }}>
+          {/* ---------------------------------Bouton menuIcon--------------------------------- */}
           <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2, display: { sm: 'none' } }}>
             <MenuIcon />
           </IconButton>
+          {/* ---------------------------------contenu qui contient le logo--------------------------------- */}
           <Box className="flex">
             <Box>
               <img src="./logo.jpg" alt="logo" className="w-[40px]" />
             </Box>
             <Box>
-              <p className="mt-1">Accèss banque</p>
+              <p className="mt-1 text-blue-900 font-bold">Accèss banque</p>
             </Box>
           </Box>
+          {/*  ---------------------------------Contenu qui contient le notification et nom utilisateur --------------------------------- */}
           <Box className="ml-auto flex">
-            <p className="text-3xl flex"><IoIosNotifications className="mt-1" />
-            |</p><Chip avatar={<Avatar />} label="anarana" className="mt-1" />
+            <p className="text-3xl flex">
+              <Badge badgeContent={2} max={10} color="error" className="mt-1 me-5">
+                <IoIosNotifications />
+              </Badge> | 
+            </p>
+            <Chip avatar={<Avatar />} label="anarana" className="mt-1" />
           </Box>
         </Toolbar>
       </AppBar>
+      {/*  ---------------------------------Contenu du sidebar --------------------------------------- */}
       <Box component="nav" sx={{ width: { sm: drawerMarginTop }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
+      {/*  ---------------------------------sidebar version mobile --------------------------------------- */}
         <Drawer  container={container} variant="temporary" open={mobileOpen} onTransitionEnd={handleDrawerTransitionEnd}
           onClose={handleDrawerClose} sx={{ display: { xs: 'block', sm: 'none' },
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth},
@@ -115,23 +126,24 @@ function NavBar(props) {
             },
           }}
         >
+          {/*  ---------------------------------données qui sont affiché dans le sidebar --------------------------------- */}
           {drawer}
         </Drawer >
-
-        <Drawer
-          variant="permanent"
+        {/*  ---------------------------------sidebar écran large --------------------------------------- */}
+        <Drawer variant="permanent"
           sx={{
             display: { xs: 'none', sm: 'block' },
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, marginTop: drawerMarginTop, borderTop: "1px solid #e0e0d1", borderTopRightRadius: "10px", boxShadow: 5 }
           }}
           open
         >
+          {/*  ---------------------------------données qui sont affiché dans le sidebar --------------------------------- */}
           {drawer}
         </Drawer>
-
       </Box>
       <Box component="main" sx={{ flexGrow: 1, p: 3, width: { xs: "100%", sm: "80%", md: "75%", lg: "70%" } }}>
         <Toolbar/>
+        {/*  ---------------------------------élément affiché dans le corps de l'application --------------------------------- */}
         <Outlet />
       </Box>
     </Box>

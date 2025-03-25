907c979b50e931c5d5ade3663d19f0539c58737a
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-03-25 21:52:45 +0300

    AJOUT MENU

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import NavBar from "./app/navigation/NavBar";
-import User from "./app/utilisateur/User";
-import Dashboard from "./app/dashboard/Dashboard";
-import Demande from "./app/demande/Demande";
-import Validation from "./app/validation/Validation";
-import Livraison from "./app/livraison/Livraison";
-import Suivi from "./app/suivi/Suivi";
-import Historique from "./app/historique/Historique";
-import Analyse from "./app/analyse/Analyse";
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Menu from './app/menu/Menu';
+import NavBar from './app/navigation/NavBar';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="demande" element={<Demande />} />
-        <Route path="validation" element={<Validation />} />
-        <Route path="livraison" element={<Livraison />} />
-        <Route path="suivi" element={<Suivi />} />
-        <Route path="historique" element={<Historique />} />
-        <Route path="analyse" element={<Analyse />} />
-        <Route path="user" element={<User />} />
-      </Routes>
-    </Router>
-  );
+   <Router>
+    <Routes>
+      <Route path="/" element={<Menu/>}/>
+      <Route path="/navbar" element={<NavBar/>}/>
+    </Routes>
+   </Router>
+  )
 }
 
-export default App;
+export default App

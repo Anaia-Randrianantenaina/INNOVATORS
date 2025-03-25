f924ab9b851c77b1fe77520726bbc6261c4edb59
Andrianary <laurentantonio68@gmail.com>
2025-03-25 11:44:04 +0300

    Changement NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NavBar from "./app/navigation/NavBar"
+import User from "./app/utilisateur/User"
+import Dashboard from "./app/dashboard/Dashboard"
 
 function App() {
   return (
     <div>
       <Router>
+        <NavBar />
         <Routes>
-          <Route path="/" element={ <NavBar/> } />
+          <Route path="/dashboard" element={ <Dashboard/> } />
+          <Route path="/user" element={ <User/> } />
         </Routes>
       </Router>
     </div>

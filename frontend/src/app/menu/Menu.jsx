87375b571ea97b84351cff4f9b90e34f655b469b
rakotomampiandry <rakotomampiandry@gmail.com>
2025-03-26 01:24:14 +0300

    finalisation navbar

--- frontend/src/app/menu/Menu.jsx
+++ frontend/src/app/menu/Menu.jsx
@@ -4,7 +4,7 @@ import { Avatar, Badge, Chip } from "@mui/material";
 
 const Menu = () => {
   return (
-    <div className="fixed top-0 right-0 bg-white w-[200px] h-[45px] mt-2 mr-4 rounded-full flex justify-end items-center px-4">
+    <div className="fixed top-0 right-0 bg-white w-[200px] h-[45px] mt-2 mr-4 rounded-full flex justify-end items-center px-4 shadow-sm">
       <span className="border rounded-full w-[50px] flex justify-center items-center">
         <Badge badgeContent={1} color="error">
           <IoMdNotificationsOutline className="text-[25px] text-gray-500" />

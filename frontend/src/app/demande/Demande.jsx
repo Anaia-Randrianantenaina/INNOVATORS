7feb8deaee12f04303b0775ce4c93f838f27610f
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-03-26 02:33:37 +0300

    Ajout composant tableau

--- frontend/src/app/demande/Demande.jsx
+++ frontend/src/app/demande/Demande.jsx
@@ -1,12 +1,30 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from '@mui/material';
+import React from 'react';
+import DataTable from '../datatable/DataTable';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Nom', width: 150 },
+  { field: 'email', headerName: 'Email', width: 150 },
+  { field: 'telephone', headerName: 'Téléphone', width: 150 },
+  { field: 'role', headerName: 'Rôle', width: 180 },
+];
+
+const rows = [
+  { id: 1, name: 'Rakoto', email: 'rakoto@gmail.com', telephone: '0340011124', role: 'Responsable logistique' },
+  { id: 2, name: 'Jean', email: 'jean@gmail.com', telephone: '0340022233', role: 'Technicien' },
+  { id: 3, name: 'Paul', email: 'paul@gmail.com', telephone: '0340033344', role: 'Manager' },
+  { id: 4, name: 'Sophie', email: 'sophie@gmail.com', telephone: '0340044455', role: 'Assistante RH' },
+  { id: 5, name: 'Luc', email: 'luc@gmail.com', telephone: '0340055566', role: 'Développeur' },
+];
 
 const Demande = () => {
   return (
-    <Box className='flex-1 p-4'>
+    <Box className="flex-1 p-4 space-y-10">
       <h1>Demande page</h1>
+      <DataTable columns={columns} rows={rows} pageSize={5} />
     </Box>
-  )
-}
+  );
+};
 
-export default Demande
+export default Demande;
